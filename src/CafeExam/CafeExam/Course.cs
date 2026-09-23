67db298231b9972215a3a24630046f83d33487ee
teta-svg <prostonastja33@gmail.com>
2026-09-23 15:22:20 +0300

    создан класс Course для хранения данных о блюдах в кафе

---
+++ src/CafeExam/CafeExam/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace CafeExam
+{
+    public class Course
+    {
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public int Duration { get; set; }
+    }
+}
